db69b11e4c98f7c8b7735a973eb7d598b86f48db
fernandoborel <fernandomenezesborel@gmail.com>
2025-06-10 21:36:37 -0300

    Inicio do projeto

---
+++ src/PedidosApp.API/Entities/Pedido.cs
@@ -0,0 +1,10 @@
+﻿namespace PedidosApp.API.Entities;
+
+public class Pedido
+{
+    public Guid? Id { get; set; }
+    public DateTime? DataHora { get; set; }
+    public decimal Valor { get; set; }
+    public string? Cliente { get; set; }
+    public string? Detalhes { get; set; }
+}
---
+++ src/PedidosApp.API/Repositories/PedidoRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using PedidosApp.API.Entities;
+
+namespace PedidosApp.API.Repositories;
+
+/// <summary>
+/// Classe de repositório para operações no banco de dados.
+/// </summary>
+public class PedidoRepository
+{
+    private readonly string _connectionString;
+
+    public PedidoRepository(string connectionString)
+     => _connectionString = connectionString;
+
+    public async Task Inserir(Pedido pedido)
+    {
+        var query = @"INSERT INTO PEDIDO(ID, DATAHORA, VALOR, CLIENTE, DETALHES)
+                      VALUES(@Id, @DataHora, @Valor, @Cliente, @Detalhes)";
+
+        using var connection = new SqlConnection(_connectionString);
+        await connection.ExecuteAsync(query, pedido);
+    }
+}
